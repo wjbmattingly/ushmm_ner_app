f813628f7cc0caf511a954fa00edd875c596d0ea
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2021-07-26 12:42:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,41 +5,57 @@ from spacy.language import Language
 models = ["ushmm_sm"]
 
 default_text = '''
-DAVID KOCHALSKI July 28, 1994 Q: I need you to start off by telling me your name, place of birth, and date of birth?
-A: My name David Kochalski.
-I was born in a small town called _____________, and I was born May 5, 1928.
-Q: Tell me a little bit about your family life before the war?
-A: Well, we were very hard working, six children, father and mother and we had a small mill, flour, buckwheat.
-We were not prosperous but comfortable.
-Q: Did you go to a public school, a private school?
-A: I went to two schools.
-One was a public school in the morning.
-In the afternoon I went to a religious school until almost late at night.
-Q: So your family was fairly religious?
-A: Yes.
-Q: And Judaism was important to you?
-A: Well, I raised in the spirit of Judaism.
-Q: Now, when you went to the public school you had a lot of friends who were not Jewish?
-A: No, the school itself, in this little city, was segregated between Catholics and Jews.
-Mind you, it was a small town, and I would say the majority of the people in that small town were Jewish people.
-Inside the town, somehow, I don't know why, but they separated us Jewish children and Catholic children.
-As you know, most of the people in Poland were Catholic.
-Q: Did you have friends who were Catholics?
-A: Yes, I used to have friends.
-Q: Did you feel any anti-semitism growing up?
-A: Yes, I did.
-I felt it, maybe not personally, but I knew of a lot of incidents whereby either they were small little -- I would call it -- we were separated, in other words, but hardly got together, and there were incidents.
-Incidents, not pleasant incidents, because we were 2 called in from the house, people regardless of how religious we were, did not believe that we were really religious people.
-Q: Were there any incidents with your brothers or sisters that you remember?
-A: No, other than we were a lovely family.
-Q: Were there a lot of cultural or social organizations, political organizations in the town?
-A: Yes there were Zionists in the city.
-There were Socialists, there were Communists, even though at that time Communists had to go underground, and religious institutions.
-Q: Were your parents active in the Zionist movement?
-A: My parents were, I would say, Zionists, and very religious.
-Q: What did you know about Hitler and the rise of Nazism before the war?
-A: I was aware what was going on even though I was a very young youngster.
-My father used to subscribe to many newspapers, Jewish in origin and non-Jewish papers, papers tha
+I would bring it home at night so we had it.
+And my grandfather was getting weaker and weaker.
+He couldn't eat.
+This was really awful, so we kept on saving his bread.
+And his wish was to live so long that we can pick up his rations.
+This was his inheritance because he said he lost all his gold, he can't leave us anything else.
+What we did he died the day before the rations came, and we didn't report it until the following day, we picked it up and then I reported the death, so we had this extra little food and an extra loaf of bread, which later I left if in Auschwitz because we're still hoarding and we're still afraid to let go and eat everything a once.
+But the existence in Lodz Ghetto was awful.
+They also had the criminal police, _______, this was German police.
+They had their headquarters in a little church, and they would call people in and say what do you have?
+Our landlord, he was the head of fences before the war.
+A real, very unsavory character, but he had a heart of gold.
+And he must have had money.
+They beat him to death.
+They let him out, he died a day later.
+But he was like -- his face was like liver.
+He never told them what he had.
+They called my mother in.
+They said we heard you are a rich dentist from __________.
+What do you have?
+And she said, "Well, I have a couple of gold chains, I have a little diamond."
+And they said, bring it in.
+So, she 14 brought it in and they let her go.
+So, they were satisfied.
+If she would have said she had nothing, they would have tortured her.
+This between the ___________ and the Jews under commando, this was pretty tight situation over there.
+And then they started evacuations.
+They were evacuating constantly people.
+One of the worst actions was when they took the children out.
+Orders came that they have to take the children, they're going to take them to better places.
+There will be fresh air and farms and the people put signs on it and they mobilized all the Jewish police including all the people who worked for the health department, including my mother, and they had to go and collect the children.
+The children were being taken voluntarily by the parents, taken down, because they couldn't hide them.
+The moment the family would hide a child, the rations would be taken away, so not only the child didn't have the rations, but the family wouldn't have the rations.
+Our next door neighbors had hidden a child and they struggled.
+Unfortunately she died later, but it was -- and then they were taking the old people.
+They would make you line up on the street and get out and just select the old people and the children Now, one of the worst actions was around the Jewish holidays, this was '44, I believe.
+And we lived on the corner, and the lady the one who was our so-called landlady, she was the wife of the guy who was killed.
+Before the war she was in Argentina.
+So, we started lining up on one street and she called everybody up and she said forget it.
+This is not our street.
+Our address is the next street.
+There were two entrances there.
+She said, all of you go back home and hide.
+Just sit quiet, don't open the window, don't stand next to windows.
+At that time she had hidden some children and my grandparents.
+We didn't know where they were, somewhere.
+They had all kinds of nooks and crannies in that building, and we had all hidden quietly, and at noon they rang the whistles and they stopped the action.
+So, they were ready to call that second street, where we were, they called it off, so we were saved.
+If she would have -- we walked out and somebody said to us, someone who knew us and said, where are your parents, and my mother said, hidden.
+He said, that's silly, they'll find them.
+They'll kill them.
 '''
 visualizers = ["ner"]
 spacy_streamlit.visualize(models, default_text, visualizers=visualizers)
